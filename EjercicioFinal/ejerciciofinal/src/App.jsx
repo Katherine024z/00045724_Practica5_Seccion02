af0075e458136127fdff31ea32414546bc6b57a4
Katherine024z <00045724@uca.edu.sv>
2025-10-03 20:07:40 -0600

    "feat: agregar ejercicio final"

---
+++ EjercicioFinal/ejerciciofinal/src/App.jsx
@@ -0,0 +1,23 @@
+import CardScientist from './Components/CardScientist';
+import { scientists } from './Data/scientists';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Cientificos notables</h1>
+      {scientists.map((scientist, index) => (
+        <CardScientist
+          key={index}
+          name={scientist.name}
+          image={scientist.image}
+          profession={scientist.profession}
+          awards={scientist.awards}
+          discoveries={scientist.discoveries}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ EjercicioFinal/ejerciciofinal/src/Data/scientists.js
@@ -0,0 +1,19 @@
+import curie from '../assets/curie.jpg';
+import katsuko from '../assets/katsuko.jpg';
+
+export const scientists = [
+  {
+    name: "Maria Skłodowska-Curie",
+    image: curie,
+    profession: "fisica y quimica",
+    awards: "4 (Premio Nobel de Física, Premio Nobel de Química, Medalla Davy, Medalla Matteucci)",
+    discoveries: "polonio (elemento quimico)"
+  },
+  {
+    name: "Katsuko Saruhashi",
+    image: katsuko,
+    profession: "geoquimica",
+    awards: "2 (Premio Miyake de geoquimica, Premio Tanaka)",
+    discoveries: "un metodo para medir el dioxido de carbono en el agua de mar"
+  }
+];
